a91b583ef403aa7898afea3a53d4405abe48f81c
Tushar Langer <tushar.langer@joulon.com>
2020-10-04 09:13:10 +0530

    callback chaining

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-geocode("New York", (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
-});
+geocode("Boston", (error, data) => {
+    if (error) {
+        return console.log("Error", error);
+    }
 
-forecast(44.1545, -75.7088, (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
+    forecast(data.latitude, data.longitude, (error, forcastData) => {
+        if (error) {
+            return console.log("Error", error);
+        }
+
+        console.log(data.location)
+        console.log(forcastData)
+    });
 });
